4009796df2a5dd2ae9aec5214d3ad9ecd5c840ed
i333463 <34474792+i333463@users.noreply.github.com>
2019-08-21 16:02:47 +0800

    update code app select

--- src/db_mysql/app.py
+++ src/db_mysql/app.py
@@ -10,17 +10,17 @@ app = Flask(__name__)
 def index() -> str:
     conn = db.get_connection()
 
-    user: {
-        "user_id": "1333463",
-        "user_name": "Eric Wu",
-        "password": "123456"
-    }
+    # user: {
+    #     "user_id": "1333463",
+    #     "user_name": "Eric Wu",
+    #     "password": "123456"
+    # }
 
-    db.create_user(conn, **user)
+    db.create_user(db.get_connection(), '1333463', 'Eric Wu', 'helloworld01')
 
-    select_user = db.select_user_by_user_id(conn, '1333463')
+    user_name = db.select_user_by_user_id(conn, '1333463')
 
-    return json.dumps({'a user inserted': select_user.get['user_name'] })
+    return json.dumps({'a user inserted': user_name })
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
--- src/db_mysql/db.py
+++ src/db_mysql/db.py
@@ -20,30 +20,28 @@ def select_user_by_user_id(conn, id):
 
     cursor.execute(query, (id,))
 
-    user = None
+    name = ''
     for (user_id, user_name) in cursor:
+        name = user_name
 
-      user : {
-          "user_id": user_id,
-          "user_name": user_name
-      }
+
     cursor.close()
 
-    return user
+    return name
 
-def create_user(conn, user):
+def create_user(conn, user_id, user_name, password):
 
     cursor = conn.cursor(buffered=True)
-    user_id = user.get["user_id"]
-    user_name = user.get["user_name"]
-    password = user.get["password"]
-    password_confirm = user.get["password_confirm"]
+    # user_id = user.get["user_id"]
+    # user_name = user.get["user_name"]
+    # password = user.get["password"]
+    # password_confirm = user.get["password_confirm"]
 
     insert_user = (
         "INSERT INTO user (user_id, user_name, password)"
         "VALUES (%s, %s, %s)")
 
-    cursor.execute(insert_user, (user.get["user_id"], user.get["user_name"], user.get["password"]))
+    cursor.execute(insert_user, (user_id, user_name, password))
 
     cursor.commit()
     cursor.close()
