1060e0e86650166401e2c3804943172639c55d75
MatthewGeekTalk <matthew.he.geek@gmail.com>
2019-08-28 17:49:27 +0800

    add db statefulset and headless service and test app deploymnet

--- src/db_mysql/app.py
+++ src/db_mysql/app.py
@@ -2,6 +2,7 @@ import db as db
 from typing import List, Dict
 from flask import Flask
 import mysql.connector
+from flask import jsonify
 import json
 
 app = Flask(__name__)
@@ -21,6 +22,13 @@ def index() -> str:
     user_name = db.select_user_by_user_id(conn, '1333463')
 
     return json.dumps({'a user inserted': user_name })
+
+@app.route("/healthz", methods=("GET", "POST"))
+def healthz():
+    """Return K8s Liveness check."""
+    resp = jsonify(success=True)
+    resp.status_code = 200
+    return resp
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
